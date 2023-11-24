c51e4d79762753105f67f881d159b6456e25f9e7
Otisco047 <133856261+Otisco047@users.noreply.github.com>
2023-11-25 11:48:32 +1300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setUp() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_pic() {
+    save("my_filter_image.png")
+}
